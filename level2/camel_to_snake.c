e783cdf520c3e7e98b71c975fcbf2ceec1b9831a
Joao Mesquita <jpmesquita29085@hotmail.com>
2025-07-17 16:12:41 +0100

    camel_to_snake

--- level2/camel_to_snake.c
+++ level2/camel_to_snake.c
@@ -2,23 +2,30 @@
 
 int is_upper(char c)
 {
-    return(c >= 'A' && c <= 'Z');
+    return (c >= 'A' && c <= 'Z');
+}
+
+int is_lower(char c)
+{
+    return (c >= 'a' && c <= 'z');
 }
 int main(int ac, char **av)
 {
     int i = 0;
+
     if (ac == 2)
     {
         while(av[1][i])
         {
-            if(is_upper(av[1][i]))
-            {   
-                if (i != 0)
-                    write(1, "_", 1);
+            if(is_lower(av[1][i]))
+                write(1, &av[1][i], 1);
+            else if(is_upper(av[1][i]))
+            {
+                write(1, "_", 1);
                 av[1][i] += 32;
+                write(1, &av[1][i], 1);
             }
-            write(1, &av[1][i], 1);
-            i++; 
+            i++;
         }
     }
     write(1, "\n", 1);
