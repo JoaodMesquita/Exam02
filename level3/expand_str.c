9b32d477f320437dedc0a517f2bb89294b48f3de
Joao Pedro De Mesquita Fernandes <joapedro@c2r3s2.42porto.com>
2025-07-25 12:40:20 +0100

    expand_str

--- level3/expand_str.c
+++ level3/expand_str.c
@@ -1,25 +1,35 @@
 #include <unistd.h>
-int is_space(char c )
+
+int is_space(char c)
 {
-    return (c == 32 || c == 9);
-}
-int main(int ac, char **av)
-{
-    int i = 0;
-    if (ac == 2)
-    {
-        while (is_space(av[1][i]))
-                i++;
-        while(av[1][i])
-        {
-                if (!is_space(av[1][i]))
-                    write(1, &av[1][i], 1);
-                else if(!is_space(av[1][i + 1]) && av[1][i + 1] != '\0')
-                    write(1, "   ", 3);
-                i++;
-        }
-    }
-    write(1, "\n", 1);
+	return(c == 32 || c == 9);
 }
 
-//" this        time it      will     be    more complex  . " 
+int main(int ac, char **av)
+{
+	int i = 0;
+
+	if (ac == 2)
+	{
+			while(is_space(av[1][i]))
+				i++;
+			while(av[1][i] || is_space(av[1][i]))
+			{
+				if (!is_space(av[1][i]))
+					write(1, &av[1][i], 1);
+				else if (is_space(av[1][i]))
+				{
+					while (is_space(av[1][i]))
+						i++;
+					if (av[1][i] == '\0')
+						break ;
+					else
+						write(1, "   ", 3);
+					i--;
+				}
+				i++;
+			}
+	}
+	write(1 , "\n", 1);
+
+}
