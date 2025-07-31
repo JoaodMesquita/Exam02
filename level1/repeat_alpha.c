bb738346225d86cc295f3722eb1ed22a28fc14c5
Joao Mesquita <jpmesquita29085@hotmail.com>
2025-07-31 12:08:16 +0100

    fixed

--- level1/repeat_alpha.c
+++ level1/repeat_alpha.c
@@ -1,34 +1,46 @@
 #include <unistd.h>
 
-int main(int ac, char **av)
+int is_lower(char c)
 {
-	int i = 0;
-	int j;
+    return(c >= 'a' && c <= 'z');
+}
+int is_upper(char c)
+{
+    return(c >= 'A' && c <= 'Z');
+}
 
-	if (ac == 2)
-	{
-		while(av[1][i])
-		{
-			if(av[1][i] >= 'a' && av[1][i] <= 'z')
-			{
-				j = 0;
-				while(j < av[1][i] - 96)
-				{
-					write(1, &av[1][i], 1);
-					j++;
-				}
-			}
-			else if(av[1][i] >= 'A' && av[1][i])
-			{
-				j = 0;
-				while(j < av[1][i] - 64)
-				{
-					write(1, &av[1][i], 1);
-					j++;
-				}
-			}
-			i++;
-		}
-	}
-	write(1, "\n", 1);
+int main (int ac, char **av)
+{
+    int i = 0;
+    int j;
+    if (ac == 2)
+    {
+        while(av[1][i])
+        {
+            if (is_lower(av[1][i]))
+            {
+                j = 0;
+                while (j < av[1][i] - 96)
+                {
+                    write(1, &av[1][i], 1);
+                    j++;
+                }
+            }
+            else if (is_upper(av[1][i]))
+            {
+                j = 0;
+                while (j < av[1][i] - 64)
+                {
+                    write(1, &av[1][i], 1);
+                    j++;
+                }
+            }
+            else
+            {
+                write(1, &av[1][i], 1);
+            }
+            i++; 
+        }
+    }   
+    write(1, "\n", 1);
 }
