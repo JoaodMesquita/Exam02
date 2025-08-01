fe5f33dd693371034f212334ec8a1cd74565f0d8
Joao Pedro De Mesquita Fernandes <joapedro@c2r3s2.42porto.com>
2025-08-01 10:22:20 +0100

    print_hex

---
+++ level3/print_hex.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+
+int	ft_atoi(char *str)
+{	
+	int i = 0;
+	int res = 0;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		res = res * 10 + (str[i] - '0');
+		i++;
+	}
+	return (res);
+}
+
+void	put_hex(int nbr)
+{
+	char *digits = "0123456789abcdef";
+
+	if (nbr >= 16)
+		put_hex(nbr / 16);
+	nbr = digits[nbr % 16];
+	write(1, &nbr, 1);
+}
+
+int main (int ac, char **av)
+{
+	if (ac == 2)
+	{
+		put_hex(ft_atoi(av[1]));
+	}
+	write(1, "\n", 1);
+	return (0);
+}
